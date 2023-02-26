5c3483ecfc87d1382364b9c0ce9e81e546dc1932
lowg <bombird.dtb@gmail.com>
2023-02-27 00:35:08 +0900

    change variable name

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import * as Core from '@actions/core'
 import Turndown from 'turndown'
 import { gfm } from 'turndown-plugin-gfm'
 
-const htmlText = Core.getInput('text')
+const htmlText = Core.getInput('html-text')
 
-Core.info('Input text: ' + htmlText)
+Core.info('Input HTML Text: ' + htmlText)
 
 const markdownText = new Turndown().use(gfm).turndown(htmlText)
 
-Core.setOutput('text', markdownText)
+Core.setOutput('markdown-text', markdownText)
 
-Core.info('Output text: ' + htmlText)
+Core.info('Output Markdown Text: ' + htmlText)
